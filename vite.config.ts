d6c35804ed803ecc41b9b417ddd9c28286696c9c
MADS-Computer <computer@multiagentdronesystems.com>
2025-06-13 10:02:45 +0000

    setup github actions

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/multiagentdronesystems.com/', // must match your GitHub repo name
+  base: './', // must match your GitHub repo name
   plugins: [react()],
   build: {
     outDir: 'dist', // stay consistent with deploy.yml
